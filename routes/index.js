f59f149a702afcd6c4a0f6effe2de708b586f412
Andrew Smith <meuzishun@gmail.com>
2023-05-10 18:30:57 -0400

    Clean up index route

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,18 @@
 const router = require('express').Router();
+const { signUpForm, signUpUser } = require('../controllers/signUpController');
+const { signInForm, signInUser } = require('../controllers/signInController');
+const {
+  newMessageForm,
+  newMessage,
+} = require('../controllers/newMessageController');
 
 /* GET test */
 router.get('/', (req, res) => {
   res.send('Home');
 });
 
-router.get('/sign-up', (req, res) => {
-  res.send('Sign up');
-});
-
-router.post('/sign-up', (req, res) => {});
-
-router.get('/sign-in', (req, res) => {
-  res.send('Sign in');
-});
-
-router.post('/sign-in', (req, res) => {});
-
-router.get('/new-message', (req, res) => {
-  res.send('New message');
-});
-
-router.post('/new-message', (req, res) => {});
+router.route('/sign-up').get(signUpForm).post(signUpUser);
+router.route('/sign-in').get(signInForm).post(signInUser);
+router.route('/new-message').get(newMessageForm).post(newMessage);
 
 module.exports = router;
