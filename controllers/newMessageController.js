731824aa485c03cb96ed38113833ee8bd40fc5c6
Andrew Smith <meuzishun@gmail.com>
2023-05-16 12:20:07 -0400

    Create newMessage controller

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -1,9 +1,22 @@
+const Message = require('../models/message');
+
 const newMessageForm = (req, res) => {
   res.render('newMessageForm', { user: req.user });
 };
 
-const newMessage = (req, res) => {
-  console.log(req.body);
+const newMessage = async (req, res, next) => {
+  try {
+    const message = new Message({
+      author: req.user.id,
+      text: req.body.content,
+      timestamp: new Date(),
+    });
+    const result = await message.save();
+    console.log(result);
+    res.render('index', { user: req.user });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 module.exports = {
